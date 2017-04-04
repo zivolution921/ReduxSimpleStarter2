850549805cca52248e4f31355d505346dc02c9df
zivolution921 <ziv921@yahoo.com>
2017-04-04 11:11:47 +0300

    Passing App and instantiating it

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import ReactDOM from 'react-dom';
 const App = function() {
   return <div>Hi!</div>;
 }
-// Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(App);
+
+ReactDOM.render(<App />);
+  
