ce8301ed8b8188b47f7fe003c9e5a6e7c8f45985
zivolution921 <ziv921@yahoo.com>
2017-04-09 11:50:16 +0300

    installed youtube search package and youtube API key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// API key that enable us to make a request to youtube
+const API_KEY = 'AIzaSyDlI8WdIrakp2rASGsj7dA7RWgHfg33Ht4';
+
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
   return <div>Hi!</div>;
 }
 
+// Create a new component. This component should produce some HTML.
+
 ReactDOM.render(<App />, document.querySelector('.container'));
-  
+
+
+
+
