eb6001e5cbdd393d19a4adf62173589589891a1c
zivolution921 <ziv921@yahoo.com>
2017-04-09 12:14:26 +0300

    exported youtube search bar and created new component to be used

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,10 @@
-search_bar.js
+import React from 'react';
+
+// declaring new componenet searchbar that contains just an input
+const SearchBar = () => {
+  return <input />;
+}
+
+
+//any file that import search bar will get that component.
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// importing that component of search bar
+import SearchBar from './components/search_bar';
+
 // API key that enable us to make a request to youtube
 const API_KEY = 'AIzaSyDlI8WdIrakp2rASGsj7dA7RWgHfg33Ht4';
 
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Create a new component. This component should produce some HTML.
