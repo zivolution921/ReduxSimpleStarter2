3ce42f2e28268d83321d319e359bd448577fa028
zivolution921 <ziv921@yahoo.com>
2017-04-15 12:18:25 +0300

    More refracoring for class component of videos

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ import SearchBar from './components/search_bar';
 // API key that enable us to make a request to youtube
 const API_KEY = 'AIzaSyDlI8WdIrakp2rASGsj7dA7RWgHfg33Ht4';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
+// whenever the app boots up we get a class of App the constructor will run right away and that will immediately kick off a search with the term surfboards. Then the callback function with data will be called with the list of videos.
+// the object that we are passing has a key of videos and the value is videos that is passed as an argument.
+// we can condesne this using ES6 when key and variable name are the same we can just use the value name which is videos in this case, for exmaple:
+// this.setState({ videos: videos }); is the same as: this.setState({ videos });
 // Create a new component. This component should produce some HTML.
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
