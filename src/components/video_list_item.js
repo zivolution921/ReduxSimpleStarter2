517e2a12d65ba2736e935250911e1a215ae27251
zivolution921 <ziv921@yahoo.com>
2017-04-22 19:34:17 -0400

    customizing a template with bootstrap

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   //const video = props.video;
-  return <li>Video</li>
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+
+          </div>
+        </div> 
+      </div>
+    </li>
+  ); 
 };
 
 export default VideoListItem;
